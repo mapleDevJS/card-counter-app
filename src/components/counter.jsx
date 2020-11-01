e6540a6182220090992301f98a405f09bcc4243a
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-10-31 21:49:04 -0400

    feat: add updating state
    Updating the State

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,7 +7,9 @@ class Counter extends Component {
     }
 
     handleIncrement = () => {
-        console.log('Increment Clicked', this);
+        this.setState({
+            count: this.state.count + 1
+        });
     }
 
     render() {         
