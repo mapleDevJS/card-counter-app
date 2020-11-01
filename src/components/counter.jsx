57d023ede1d018ed925f34c88268f6da96f5c8d4
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-10-31 21:30:15 -0400

    feat: render list
    Rendering Lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     }
 
     // styles = {
@@ -15,6 +16,7 @@ class Counter extends Component {
         <>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
+            <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
         </>);
     }
 
