07642ba7e8f35b50c58d4db7143ce131f18d1dca
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-10-31 21:19:53 -0400

    feat: add styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,11 +5,16 @@ class Counter extends Component {
         count: 0
     }
 
+    // styles = {
+    //     fontSize: 50,
+    //     fontWeight: 700
+    // };
+
     render() { 
         return (
         <>
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+            <span className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <button className='btn btn-secondary btn-sm'>Increment</button>
         </>);
     }
 
