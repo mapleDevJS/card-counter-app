d6598e1fdaedc20c5d4842caaef5c2d719634040
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-11-01 18:54:21 -0500

    feat: updated structure
    Multiple Components in Sync

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
+import './App.css';
 
 
 ReactDOM.render(
-  <Counters />,
+  <App />,
   document.getElementById('root')
 );
 
