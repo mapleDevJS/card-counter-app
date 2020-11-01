b3a4c6b1f6c73579544ac671ad3c4542e5c90f92
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-11-01 08:06:39 -0500

    feat: added reset button
    Single Source of Truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -12,13 +12,27 @@ class Counters extends Component {
   };
 
   handleDelete = (counterId) => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({counters});
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
   };
+
+  handleReset = () => {
+      const counters = this.state.counters.map(c => {
+          c.value = 0;
+          return c;
+      });
+      this.setState({counters});
+  }
 
   render() {
     return (
       <div>
+        <button
+          onclick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+            Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
